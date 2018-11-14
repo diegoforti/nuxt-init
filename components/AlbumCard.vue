d402c66f7e12856fe634a87b3c705cb849d6202b
Diego Forti <diego-rolando.forti@atos.net>
2018-11-14 16:00:02 -0300

    Se sube por promera vez

---
+++ components/AlbumCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="column is-one-quarter">
+        <div class="box">
+            <h3>{{album.title}}</h3>
+            <nuxt-link :to="`/albums/${album.id}`" class="button is-primary">ver álbum</nuxt-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'AlbumCard',
+  props: {
+    album: Object
+  }
+}
+</script>
+
+<style>
+.box {
+  padding: 1em;
+}
+h3 {
+  min-height: 5em;
+}
+</style>
